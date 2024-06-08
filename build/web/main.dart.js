9dc41160c58f78edde31fc8ba2d2534a79537e0d
Monageng <monageng2010@gmail.com>
2024-06-08 17:41:04 +0000

    Build Flutter artifacts - Build #2

--- build/web/main.dart.js
+++ build/web/main.dart.js
@@ -28452,7 +28452,7 @@ $1(a){var s=$.cz
 s=(s==null?$.cz=A.fn(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/3f3e560236539b7e2702f5ac790b2a4691b32d49/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/f40e976bedff57e69e1b3d89a7c2a3c617a03dad/":s)+a},
 $S:68}
 A.awa.prototype={
 $1(a){this.a.remove()
